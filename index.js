68c91f3354e15416c95888fa0131901e0647c35a
hinanshisuthar <hinanshis4@gmail.com>
2022-02-26 10:14:21 +0530

    2

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ import './style.css';
 // Write Javascript code!
 
 //1
-const strLength = (name, cbFunction) => {
-  cbFunction(name.length);
+// const strLength = (name, cbFunction) => {
+//   cbFunction(name.length);
+// };
+
+// const printName = (nameLength) =>
+//   console.log(`omg my name is ${nameLength} chars long!!!`);
+
+// strLength('hinanshi', printName);
+
+//2
+const willThanosKillMe = (name, noKill, kill) => {
+  if (name.length % 2 === 0) {
+    noKill();
+  } else {
+    kill();
+  }
 };
 
-const printName = (nameLength) =>
-  console.log(`omg my name is ${nameLength} chars long!!!`);
+const noKill = () => console.log('Yay! I am Alive!');
 
-strLength('arpan', printName);
+const kill = () =>
+  console.log('Give my bose speakers and apple headphones to my sister');
+
+willThanosKillMe('hinanshi', noKill, kill);
