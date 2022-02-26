c9119c07a0b3a231ab1e2704ede80ba09ade3652
hinanshisuthar <hinanshis4@gmail.com>
2022-02-26 10:25:38 +0530

    3

--- index.js
+++ index.js
@@ -14,17 +14,26 @@ import './style.css';
 // strLength('hinanshi', printName);
 
 //2
-const willThanosKillMe = (name, noKill, kill) => {
-  if (name.length % 2 === 0) {
-    noKill();
-  } else {
-    kill();
-  }
-};
+// const willThanosKillMe = (name, noKill, kill) => {
+//   if (name.length % 2 === 0) {
+//     noKill();
+//   } else {
+//     kill();
+//   }
+// };
 
-const noKill = () => console.log('Yay! I am Alive!');
+// const noKill = () => console.log('Yay! I am Alive!');
 
-const kill = () =>
-  console.log('Give my bose speakers and apple headphones to my sister');
+// const kill = () =>
+//   console.log('Give my bose speakers and apple headphones to my sister');
 
-willThanosKillMe('hinanshi', noKill, kill);
+// willThanosKillMe('hinanshi', noKill, kill);
+
+//3
+// const myGreeting = () => {
+//   console.log('henlo jhonu');
+// };
+// console.log('henlo jhonu 1');
+// const myTimeout = setTimeout(myGreeting, 0);
+
+//setTimeout(fn, 0) is used when one needs to do calculation when the callstack is empty. Therefore here, there is no blocking of render.
